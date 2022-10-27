2528c5b7702cb2117d38b94c877ae2e649132587
aslettz <zankaslett@gmail.com>
2022-10-26 21:06:27 -0600

    about me component

---
+++ components/aboutMe.tsx
@@ -0,0 +1,19 @@
+
+const AboutMe = () => {
+
+    return (
+        <div id={'about-me'}>
+            <h1>About Me</h1>
+            <p>
+                This is where I will put a lot of information about me. It will
+                make me look cool and also very personable probably. People
+                should want to be my friend and hire me after reading all of
+                this. It's a good thing I can build a website!
+                (Don't forget a picture of myself!)
+            </p>
+        </div>
+    );
+
+}
+
+export default AboutMe
