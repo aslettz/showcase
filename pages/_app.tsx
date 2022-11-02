534a1940571ccc9db73b58af9c7350d783bb6bbe
ZanAslett <zankaslett@gmail.com>
2022-11-02 15:42:12 -0600

    Cleaning

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,7 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { WagmiConfig, createClient } from 'wagmi'
 import { getDefaultProvider } from 'ethers'
-import Header from "../components/header";
+import Head from "next/head";
 
 const client = createClient({
   autoConnect: false,
@@ -11,12 +10,14 @@ const client = createClient({
 
 function MyApp({ Component, pageProps }: AppProps) {
   return(
-      // <AnimatePresence>
-          <WagmiConfig client={client}>
-              <Header/>
-              <Component {...pageProps} />
-          </WagmiConfig>
-      // </AnimatePresence>
+      <WagmiConfig client={client}>
+          <Head>
+              <title>Zan Aslett</title>
+              <meta name="description" content="Portfolio site for Zan Aslett"/>
+              <link rel="icon" href="../public/favicon.ico"/>
+          </Head>
+          <Component {...pageProps} />
+      </WagmiConfig>
   )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,58 +1,12 @@
 import type {NextPage} from 'next'
-import Head from 'next/head'
-import Header from '../components/header'
-import styles from '../styles/Home.module.css'
-import {motion} from "framer-motion";
-import InfoSection, {InfoSectionTypes} from "../components/infoSection";
-import styled from "styled-components";
-import {Element} from "react-scroll/modules";
-
-const Main = styled.div`
-  padding: 4rem 0;
-  flex: 1;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;  
-`
-
-const BigElement = styled(Element)`
-  min-height: 80vh;
-`
 
 const Home: NextPage = () => {
 
-  return (
+    return (
+        <>
 
-    <div className={styles.container}>
-      <Head>
-        <title>Zan Aslett</title>
-        <meta name="description" content="Portfolio site for Zan Aslett" />
-        <link rel="icon" href="../public/favicon.ico" />
-      </Head>
-
-      <Main id={'main'} className={styles.main}>
-          <BigElement name={'Home'} className={'BigElement'}>
-              <InfoSection infoSectionType={InfoSectionTypes.Home} anchorId={'home'}/>
-          </BigElement>
-        <BigElement name={'about-me'}>
-            <InfoSection infoSectionType={InfoSectionTypes.AboutMe} anchorId={'about-me'}/>
-        </BigElement>
-          <BigElement name={'skills'}>
-              <InfoSection infoSectionType={InfoSectionTypes.Skills} anchorId={'skills'}/>
-          </BigElement>
-          <BigElement name={'experience'}>
-              <InfoSection infoSectionType={InfoSectionTypes.Experience} anchorId={'experience'}/>
-          </BigElement>
-          <BigElement name={'education'}>
-              <InfoSection infoSectionType={InfoSectionTypes.Education} anchorId={'education'}/>
-          </BigElement>
-          <BigElement name={'side-projects'}>
-              <InfoSection infoSectionType={InfoSectionTypes.SideProjects} anchorId={'side-projects'}/>
-          </BigElement>
-      </Main>
-    </div>
-  )
+        </>
+    )
 }
 
 export default Home;
