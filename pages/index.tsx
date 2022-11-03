a44e06ac5ef268641304f3a5f1bdb8872bcee22a
ZanAslett <zankaslett@gmail.com>
2022-11-03 10:16:43 -0600

    Section off the code

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,33 @@ import type {NextPage} from 'next'
 
 const Home: NextPage = () => {
 
+
+    function renderHeroBanner() {
+        return <div>Hero Banner</div>
+    }
+
+    function renderAboutMe() {
+        return <div>About Me</div>
+    }
+
+    function renderExperience() {
+        return <div>Experience</div>
+    }
+
+    function renderSkillsDisplay() {
+        return <div>Skills</div>
+    }
+
     return (
         <>
-
+            <div>
+                {renderHeroBanner()}
+                {renderAboutMe()}
+                {renderExperience()}
+            </div>
+            <div>
+                {renderSkillsDisplay()}
+            </div>
         </>
     )
 }
