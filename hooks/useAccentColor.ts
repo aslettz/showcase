1a709da532c028e8d07d8fc2091504780fd86e1e
aslettz <zankaslett@gmail.com>
2022-11-15 11:25:48 -0700

    Hooks cleaned up

--- hooks/useAccentColor.ts
+++ hooks/useAccentColor.ts
@@ -4,6 +4,8 @@ import appConfigVar from "../globalState";
 import contractJson from "../AslettcoToken.json";
 import axios from "axios";
 import {useState} from "react";
+import useHasNft from "./useHasNft";
+import useGetNftColor from "./useGetNftColor";
 
 export interface AccentColorConfig {
     baseAccentColor: string;
@@ -14,43 +16,16 @@ export interface AccentColorConfig {
 const useAccentColor = () : AccentColorConfig => {
     const { address, isConnected } = useAccount();
     const { useNftColor } = useReactiveVar(appConfigVar);
-    const [nftColor, setNftColor] = useState();
-    const {data, isError} = useContractReads({
-        contracts: [
-            {
-                abi: contractJson.abi,
-                address: "0xD7032E28FE313870329977a2c80E708DbA818165",
-                functionName: "balanceOf",
-                args: [address],
-            },
-            {
-                abi: contractJson.abi,
-                address: "0xD7032E28FE313870329977a2c80E708DbA818165",
-                functionName: "getTokenUriByAddress",
-                args: [address],
-            }
-        ]
-    });
+    const nftColor = useGetNftColor();
 
     const mediumOpacity = "77";
     const heavyOpacity = "22";
 
-    async function readAndSetNftColor(metadataUrl: string) {
-        const response = await axios.get(metadataUrl);
-        setNftColor(response.data.properties.color);
-    }
-
-    if (isConnected && useNftColor && data && !isError && data[0] != 0) {
-        if (!nftColor) {
-            const metadataUrl = (data[1] as string).replace("ipfs://", "https://nftstorage.link/ipfs/");
-            readAndSetNftColor(metadataUrl);
-        }
-        else {
-            return {
-                baseAccentColor: nftColor,
-                mediumAccentColor: nftColor + mediumOpacity,
-                lightAccentColor: nftColor + heavyOpacity,
-            }
+    if (isConnected && useNftColor && nftColor) {
+        return {
+            baseAccentColor: nftColor,
+            mediumAccentColor: nftColor + mediumOpacity,
+            lightAccentColor: nftColor + heavyOpacity,
         }
     }
 
@@ -59,7 +34,6 @@ const useAccentColor = () : AccentColorConfig => {
         mediumAccentColor: '#FF551B' + mediumOpacity,
         lightAccentColor: '#FF551B22' + heavyOpacity,
     }
-
 }
 
 export default useAccentColor;
--- hooks/useGetNftColor.ts
+++ hooks/useGetNftColor.ts
@@ -2,16 +2,12 @@ import {useAccount, useContractRead} from "wagmi";
 import contractJson from "../AslettcoToken.json";
 import {useState} from "react";
 import axios from "axios";
+import useHasNft from "./useHasNft";
 
-const useGetNftColor = (): string => {
-
-    async function readAndSetNftColor(metadataUrl: string) {
-        const response = await axios.get(metadataUrl);
-        setNftColor(response.data.properties.color);
-    }
-
+const useGetNftColor = (): string | null => {
     const { address, isConnected } = useAccount();
     const [nftColor, setNftColor] = useState();
+    const hasNft = useHasNft();
     const {data, isError} = useContractRead({
         abi: contractJson.abi,
         address: "0xD7032E28FE313870329977a2c80E708DbA818165",
@@ -19,13 +15,21 @@ const useGetNftColor = (): string => {
         args: [address],
     })
 
-    if (!nftColor) {
-        const metadataUrl = (data as string).replace("ipfs://", "https://nftstorage.link/ipfs/");
-        readAndSetNftColor(metadataUrl);
-        return '#FF551B';
+    async function readAndSetNftColor(metadataUrl: string) {
+        const response = await axios.get(metadataUrl);
+        setNftColor(response.data.properties.color);
     }
 
-    return nftColor
+    if (isConnected && hasNft && data && !nftColor) {
+        const metadataUrl = (data as string).replace("ipfs://", "https://nftstorage.link/ipfs/");
+        readAndSetNftColor(metadataUrl);
+        return null;
+    }
+    if (!nftColor) {
+        return null;
+    }
+
+    return nftColor;
 }
 
 export default useGetNftColor;
