4e7b2ad252df2e2d4292121297b97a4a2026d76e
aslettz <zankaslett@gmail.com>
2022-11-15 15:14:54 -0700

    Ignore problem import for now

--- pages/api/generate-image.ts
+++ pages/api/generate-image.ts
@@ -2,6 +2,7 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import path from 'path'
 import sharp from 'sharp';
 import Color from 'color';
+// @ts-ignore
 import { NFTStorage, File } from 'nft.storage';
 import {Network, Alchemy, Wallet} from "alchemy-sdk";
 import dotenv from "dotenv";
